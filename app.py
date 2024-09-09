e20218dcfc5f1790fef3d48ebe99152f7a05ce56
Kumo1e <so885617@gmail.com>
2024-09-09 17:01:28 +0800

    Update

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import cv2
 
 
 if __name__ == "__main__":
-    port = "COM6"
+    port = "COM6" # your arduino port
     camera_id = 0
     model_path = "./models/gesture_recognizer.task"
     
-    # ser = serial.Serial(port, 9600)  # 替換為你的MATRIX板子的端口
+    ser = serial.Serial(port, 9600)  # 替換為你的MATRIX板子的端口
     cap = Camera(camera_id)
     track_ball = TrackGreenBall()
     recognize = AppGestureRecognizer(model_path)
@@ -26,20 +26,20 @@ if __name__ == "__main__":
                 if turn%5 == 0:
                     print("轉圈")
                     command = f"c\n"
-                    # ser.write(command.encode())
+                    ser.write(command.encode())
                     cv2.waitKey(1000)
             elif gesture == "Thumb_Down": # 後退
                 print("後退")
                 command = f"b\n"
-                # ser.write(command.encode())
+                ser.write(command.encode())
             elif gesture == "Thumb_Up": # 前進
                 print("前進")
                 command = f"a\n"
-                # ser.write(command.encode())
+                ser.write(command.encode())
             elif gesture == "Open_Palm": # 前進
                 print("停")
                 command = f"w\n"
-                # ser.write(command.encode())
+                ser.write(command.encode())
             elif gesture == "ILoveYou": # 跟隨
                 is_follow = True
                 while is_follow:
@@ -47,19 +47,19 @@ if __name__ == "__main__":
                     move = track_ball.track_green_ball(hsv)
                     gesture = recognize.gesture_recognizer(frame)
                     command = move
-                    # ser.write(command.encode())
+                    ser.write(command.encode())
                     cv2.waitKey(10)
                     command = ""
-                    # ser.write(command.encode())
+                    ser.write(command.encode())
                     print(move)
                     if gesture == "Open_Palm":
                         is_follow = False
-                        command = f"d\n"
-                        # ser.write(command.encode())
+                        command = f"w\n"
+                        ser.write(command.encode())
             else:
                 print(gesture)
                 command = f"j\n"
-                # ser.write(command.encode())
+                ser.write(command.encode())
             cv2.waitKey(200)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
